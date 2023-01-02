76fe88e1cf6225b0acf206ac4ce9d52b360c3656
TajwarSaiyeed <rjabid36@gmail.com>
2023-01-02 10:35:28 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const socketio = require("socket.io");
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+
+const users = [{}];
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on("connection", (socket) => {
+  console.log("We have a new connection!");
+
+  socket.on("joined", ({ user }) => {
+    users[socket.id] = user;
+    console.log("joined", user);
+    socket.broadcast.emit("userJoined", {
+      user: "Admin",
+      message: `${users[socket.id]} has joined`,
+    });
+    socket.emit("welcome", {
+      user: "Admin",
+      message: `welcome to the chat ${users[socket.id]}`,
+    });
+  });
+
+  socket.on("message", ({ id, message }) => {
+    io.emit("sendMessage", { user: users[id], message, id });
+  });
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("leave", {
+      user: "Admin",
+      message: `${users[socket.id]} has left`,
+    });
+    console.log("User had left!!!");
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is running.");
+});
+
+server.listen(port, () => console.log(`Server has started on port ${port}`));
