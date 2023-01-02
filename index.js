3706edb3fd9dfa4fc962b920b0840ae9f66e30c1
TajwarSaiyeed <rjabid36@gmail.com>
2023-01-02 11:13:02 +0600

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const socketio = require("socket.io");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 app.use(cors());
 
 const users = [{}];
